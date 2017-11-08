e47d181751a7262847e94ec41895addfcf7ac5b5
vagrant <vagrant@homestead>
2017-11-08 13:19:50 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201706/27/5/7s0GwhKcOB.png" alt="">
+            <a href="https://yousails.com">
+                优账远扬|创造不息，交付不止
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
